e2bb5a0a4b1288149bb997f6279accb30bef9b11
Flic Gibbs <flic@gibbz.biz>
2024-08-16 18:10:52 +0530

    remove commented code

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,5 @@
-/*import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-// import { BrowserRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);*/
-
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-// import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 
